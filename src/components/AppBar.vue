c04c636b9ca75cce8911f819eb5502777690fbda
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-05-17 23:21:16 -0500

    barra lateral

---
+++ src/components/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My Application</v-toolbar-title>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  import { inject } from 'vue'
+  
+  export default {
+    setup() {
+      const drawer = inject('drawer');
+  
+      const toggleDrawer = () => {
+        drawer.value = !drawer.value;
+      };
+  
+      return { toggleDrawer };
+    }
+  }
+  </script>
+  
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            @click="navigateTo(item.route)"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  
+  export default {
+    setup() {
+      const drawer = ref(false);
+      const items = [
+        { title: 'Home', icon: 'mdi-home', route: '/' },
+        { title: 'About', icon: 'mdi-information', route: '/about' },
+        // Agrega más ítems según sea necesario
+      ];
+  
+      const router = useRouter();
+  
+      const navigateTo = (route) => {
+        router.push(route);
+      };
+  
+      return { drawer, items, navigateTo };
+    }
+  }
+  </script>
+  
